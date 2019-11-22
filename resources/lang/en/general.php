f9c7e7e89fb6779cad4d9fab8bad71846da9a768
Bguillenn <bguillenn11@gmail.com>
2019-11-22 05:21:51 -0500

    Version 0.0

---
+++ resources/lang/en/general.php
@@ -0,0 +1,7 @@
+<?php
+/*Translates for general APP set diferent views name*/
+return [
+    'appname' => 'VitaPet | Pet insurance service',
+    'auth' => 'Log-in or Register',
+    'main' => 'Welcome to VitaPet'
+];
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{__('general.appname')}}</title>
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/landing-page/style.css') }}" />
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+
+        </div>
+        <div class="content-pane">
+
+        </div>
+    </div>
+</body>
+</html>
